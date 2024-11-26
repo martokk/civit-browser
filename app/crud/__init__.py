cb4b6d4a3cdf4d20de8e8a92f0934f3c870a753b
martokk <25113632+martokk@users.noreply.github.com>
2024-11-25 20:19:31 -0600

    update tests

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,6 +1,17 @@
+from .base import BaseCRUD
 from .cursor import cursor
+from .exceptions import DeleteError, RecordAlreadyExistsError, RecordNotFoundError
 from .generated_image import generated_image
 from .settings import settings
 from .user import user
 
-__all__ = ["cursor", "generated_image", "settings", "user"]
+__all__ = [
+    "BaseCRUD",
+    "cursor",
+    "generated_image",
+    "settings",
+    "user",
+    "DeleteError",
+    "RecordAlreadyExistsError",
+    "RecordNotFoundError",
+]
